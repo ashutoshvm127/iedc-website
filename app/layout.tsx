254436230abe42f77a996883ef23a1dcbeccca0a
Ashutosh V M <developer.37826@gmail.com>
2025-05-29 21:15:38 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next';
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
@@ -29,6 +30,7 @@ export default function RootLayout({
             <Footer />
           </div>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
