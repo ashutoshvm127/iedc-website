d060263bd3acb0fbcaa7df75e3bbf3194f154289
Ashutosh V M <developer.37826@gmail.com>
2025-08-22 12:05:23 +0530

    Update data.tsx

--- data/data.tsx
+++ data/data.tsx
@@ -526,9 +526,9 @@ export const excomData = {
       },
     },
     {
-      name: "Harikrishnan S",
+      name: "Harikrishnan S S",
       position: "Jr. Design Lead",
-      image: "/images/excom/unknown.jpg?height=300&width=300",
+      image: "/images/execom25-26/harikrishnan.jpg?height=300&width=300",
       //change
       department: "Computer Science ( AI )",
       year: "Second Year",
